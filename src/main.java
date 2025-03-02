52f429c0a0df69a69210837d0274df247761f20d
Ibrokhimm0752 <Ibrokhimm0752@hstat.org>
2025-03-02 11:21:52 -0500

    updated

--- src/main.java
+++ src/main.java
@@ -1,5 +1,10 @@
-public class Main {
+public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("");
+        for (int i = 0; i <= 18; i += 3) {
+            {
+                System.out.println(i + " ");
+            }
+        }
     }
 }
